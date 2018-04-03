8bba0f689a0621af9d2f896464f7b3db8070eb93
SebaRaba <rabasebastians@gmail.com>
2018-04-03 11:50:52 +0100

    new iteration on data modeling

--- algorithms/big_data_modeling.py
+++ algorithms/big_data_modeling.py
@@ -10,11 +10,15 @@ from networkx.algorithms import bipartite
 
 buyers = []
 items = []
+period_lasting = []
 
-count_accuired = 0
+count_transaction = 0
 count_view = 0
-random_count = 0
+count_add_to_cart = 0
 
+count_fourteen = 0
+count_fifteen = 0
+count_others = 0
 
 # Big dataset preparation
 with open('../data/events.csv') as csvfile:
@@ -24,17 +28,51 @@ with open('../data/events.csv') as csvfile:
         if row[2] != 'transaction' and row[2] != 'view' and row[2] not in items:
             items.append(row[2])
         if row[2] == 'transaction':
-            count_accuired += 1
+            count_transaction += 1
         elif row[2] == 'view':
             count_view += 1
         else:
-            random_count += 1
+            count_add_to_cart += 1
+
+            print('HERE 1')
+    
+        if int(row[0]) % 10 == 4:
+            count_fourteen += 1
+            if row[0] not in period_lasting:
+                period_lasting.append(row[0])
+
+            print('HERE 2')
+        if int(row[0]) % 10 == 5:
+            count_fifteen += 1
+            if row[0] not in period_lasting:
+                period_lasting.append(row[0])
+
+            print('HERE 3')
+        if int(row[0]) % 10 != 5 and int(row[0]) % 10 != 4:
+            count_others += 1
+            if row[0] not in period_lasting:
+                period_lasting.append(row[0])
+
+            print('HERE 4')
+            input()
 
 
-print('Transaction in big data', count_accuired)
+print('Transaction in big data', count_transaction)
 print('Views in dataset', count_view)
-print('What\'s left', random_count)
+print('What\'s left', count_add_to_cart)
 
-for entry in items:
+print('Raport view on actual transaction:', count_view/count_transaction)
+print('Raport add to cart on transactions:', count_add_to_cart/count_transaction)
+
+print('Count fourteen:', count_fourteen)
+print('Count fifteen:', count_fifteen)
+print('Count others:', count_others)
+
+
+# Write transactions info into a new file .txt
+# Perform analisys and generate true predictions
+# Test predictions on test dataset
+# Find out timestamp format!! IT misses sense. We need to find out how much time we have recorded in the file.
+
+for entry in period_lasting:
     print(entry)
-    input()
